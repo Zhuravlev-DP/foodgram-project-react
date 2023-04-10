62ab643102f552bb54b3cc5459b88c6fa1ada33a
Zhuravlev Dmitry <Zhyravlev-dmitry97@yandex.ru>
2023-04-10 14:25:51 +0500

    Создание структуры проекта/приложений, добавлена модель User

---
+++ backend/users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_user'
+            )
+        ]
+
+    def __str__(self):
+        return self.username
